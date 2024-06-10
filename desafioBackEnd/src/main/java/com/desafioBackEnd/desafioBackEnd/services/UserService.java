0410d24540431d3f85eea2686247a27861445f6b
gerson.fragoso <gersonfragoso.araujo@gmail.com>
2024-06-10 15:35:55 -0300

    feat: create user Service method

--- desafioBackEnd/src/main/java/com/desafioBackEnd/desafioBackEnd/services/UserService.java
+++ desafioBackEnd/src/main/java/com/desafioBackEnd/desafioBackEnd/services/UserService.java
@@ -1,4 +1,35 @@
 package com.desafioBackEnd.desafioBackEnd.services;
 
+
+import com.desafioBackEnd.desafioBackEnd.domain.user.User;
+import com.desafioBackEnd.desafioBackEnd.domain.user.UserType;
+import com.desafioBackEnd.desafioBackEnd.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal value) throws Exception {
+        if (sender.getUserType() == UserType.lojista){
+            throw new Exception("Usuario do tipo lojista nao pode fazer transação");
+        }
+        if (sender.getBalance().compareTo(value) < 0){
+            throw new Exception("Saldo Insuficiente");
+        }
+
+    }
+
+    public void findUserById(Long id) throws Exception {
+        userRepository.findById(id).orElseThrow(()-> new Exception("Usuario não encontrado"));
+    }
+
+    public void saveUser(User user){
+        this.userRepository.save(user);
+    }
+
 }
