03715e3496888924a5b21850db142275423315ce
gerson.fragoso <gersonfragoso.araujo@gmail.com>
2024-06-10 20:35:08 -0300

    fix: update user

--- desafioBackEnd/src/main/java/com/desafioBackEnd/desafioBackEnd/domain/user/User.java
+++ desafioBackEnd/src/main/java/com/desafioBackEnd/desafioBackEnd/domain/user/User.java
@@ -1,20 +1,22 @@
 package com.desafioBackEnd.desafioBackEnd.domain.user;
 
-
+import com.desafioBackEnd.desafioBackEnd.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
-@Entity(name = "users")
-@Table(name = "users")
+@Entity(name = "tableUser")
+@Table(name = "tableUser")
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of="id")
+@EqualsAndHashCode(of = "id")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,8 +28,17 @@ public class User {
     @Column(unique = true)
     private String email;
     private String password;
-    private BigDecimal balance;
+    private BigDecimal amount;
     @Enumerated(EnumType.STRING)
     private UserType userType;
 
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.document = data.document();
+        this.email = data.email();
+        this.password = data.password();
+        this.amount = data.amount();
+        this.userType = data.userType();
+    }
 }
